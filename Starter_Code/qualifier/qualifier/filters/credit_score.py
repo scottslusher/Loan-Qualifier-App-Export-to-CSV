af2385cd04b14637596c11cabffa362ae743d04e
Scott Slusher <scottslusher22@gmail.com>
2021-07-01 21:38:55 -0500

    tested the filter

--- Starter_Code/qualifier/qualifier/filters/credit_score.py
+++ Starter_Code/qualifier/qualifier/filters/credit_score.py
@@ -22,3 +22,4 @@ def filter_credit_score(credit_score, bank_list):
         if credit_score >= int(bank[4]):
             credit_score_approval_list.append(bank)
     return credit_score_approval_list
+    
